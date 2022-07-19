69efc94bae35cad964b54977ac145db2bf30630e
UmutcanGuncu <92626158+UmutcanGuncu@users.noreply.github.com>
2022-07-19 19:56:07 +0300

    Kdv Hesaplama Tamamlandi

--- kdvHesaplama/src/Main.java
+++ kdvHesaplama/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner s1= new Scanner(System.in);
+        System.out.println("Urun Fiyatini Giriniz");
+        int kdvsizFiyat = s1.nextInt();
+        if(kdvsizFiyat<1000){
+            double kdvMiktar = kdvsizFiyat*0.18;
+            double kdvliFiyat = kdvsizFiyat+kdvMiktar;
+            System.out.println("Urunun kdv dahil fiyati = "+ kdvliFiyat);
+        }
+        else{
+            double kdvMiktar = kdvsizFiyat*0.8;
+            double kdvliFiyat = kdvsizFiyat+kdvMiktar;
+            System.out.println("Urunun kdv dahil fiyati = "+ kdvliFiyat);
+        }
+
     }
 }
