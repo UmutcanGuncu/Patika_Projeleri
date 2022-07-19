3815cc233cd668d1679bcb2ac204431c95b91e3d
UmutcanGuncu <92626158+UmutcanGuncu@users.noreply.github.com>
2022-07-19 20:55:19 +0300

    Manav Kasa Tamamlandi

---
+++ ManavKasa/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Elma Kilogramini Giriniz");
+        int apple = input.nextInt();
+        System.out.println("Armut Kilogramini Giriniz");
+        int pear = input.nextInt();
+        System.out.println("Domates Kilogramini Giriniz");
+        int tomato = input.nextInt();
+        System.out.println("Muz Kilogramini Giriniz");
+        int banana = input.nextInt();
+        System.out.println("Patlican Kilogramini Giriniz");
+        int aubergine = input.nextInt();
+        double appleKgPrice =3.67;
+        double pearKgPrice =2.14;
+        double tomatoKgPrice = 1.11;
+        double bananaKgPrice =0.95;
+        double aubergineKgPrice =5.00;
+        double totalPrice = (appleKgPrice*apple+pearKgPrice*pear+tomatoKgPrice*tomato+bananaKgPrice*banana
+        +aubergineKgPrice*aubergine);
+
+        System.out.println("Total Price = " +String.format("%.2f",totalPrice) + " TL" );
+
+
+    }
+}
