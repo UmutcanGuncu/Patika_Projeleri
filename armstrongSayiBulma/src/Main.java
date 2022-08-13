6a3b7c75b134cbccedd6653ebb7ab21b86317aee
UmutcanGuncu <92626158+UmutcanGuncu@users.noreply.github.com>
2022-08-13 18:03:12 +0300

    Armstrong Sayı Bulma

--- armstrongSayiBulma/src/Main.java
+++ armstrongSayiBulma/src/Main.java
@@ -6,10 +6,31 @@ public class Main {
         System.out.print("Lutfen Sayiyi Giriniz : ");
         int number = input.nextInt();
         int tempNumber = number;
-        int numberCounter =0; // basamak sayısı
+        // basamak sayısı bulma işlemi
+        int numberCounter =0;
+        int basValue ;
+
+        int result =0;
         while (tempNumber !=0){
             tempNumber/=10;
             numberCounter++;
+        }
+        tempNumber =number;
+        System.out.println("Basamak Sayisi : " + numberCounter);
+        while (tempNumber !=0){
+            basValue = tempNumber %10;
+            int basPow =1;
+             for(int i =1;i<=numberCounter;i++){
+                basPow *=basValue;
+            }
+            result += basPow;
+            tempNumber/=10;
+        }
+        if(result==number){
+            System.out.println(number+" Armstrong Sayidir");
+
+        }else {
+            System.out.println(number+" Armstrong Sayi Degildir");
         }
 
     }
