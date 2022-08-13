638b7a3e2f7fb483a92bb832733c13423beb06af
UmutcanGuncu <92626158+UmutcanGuncu@users.noreply.github.com>
2022-08-13 17:12:02 +0300

    While Döngüsü İle Ebob Ekok Bulma Tamamlandı

--- ebobEKOK/src/Main.java
+++ ebobEKOK/src/Main.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input =new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("N1 Sayisini Giriniz : ");
         int n1 = input.nextInt();
         System.out.print("N2 Sayisini Giriniz : ");
         int n2 = input.nextInt();
-        int ebob =1;
+        int ebob = 1;
+        /*
         //Ebob Bulma
         if (n1 > n2) {
             for (int i = 1; i <= n2; i++) {
@@ -27,11 +28,46 @@ public class Main {
         System.out.println(ebob);
         //Ekok Bulma
 
-        for(int i =1 ; i<=(n1*n2);i++){
-            if(i%n1==0 && i%n2==0){
+        for (int i = 1; i <= (n1 * n2); i++) {
+            if (i % n1 == 0 && i % n2 == 0) {
                 System.out.println(i);
                 break;
+            } */
+        //While Dönügüsü İle Ebob Ekok Bulma
+
+        //While İle Ebob Bulma
+        int i = 1;
+        if (n1 > n2) {
+            while (n2 != i) {
+                if (n1 % i == 0 && n2 % i == 0) {
+                    ebob = i;
+                }
+                i++;
+            }
+
+        } else {
+            while (n1 != i) {
+                if (n1 % i == 0 && n2 % i == 0) {
+                    ebob = i;
+                }
+                i++;
             }
         }
+        System.out.println(ebob);
+
+        //While ile ekok Bulma
+        int j = 1;
+        int ekok =1;
+        while (j > 0) {
+            if (j % n1 == 0 && j % n2 == 0) {
+                ekok = j;
+                break;
+            }
+            j++;
+
+        }
+        System.out.println(ekok);
+
     }
 }
+
