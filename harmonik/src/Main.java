3e717c15d48bb2248d6b8c1a60d09e8e20d94754
UmutcanGuncu <92626158+UmutcanGuncu@users.noreply.github.com>
2022-08-13 18:44:48 +0300

    Girilen sayiya kadar olan harmonik seriyi bulma programı tamamlandi.

---
+++ harmonik/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double result =0.00;
+        System.out.print("Harmonik Serisini Bulmak İstedigiinz Sayiyi Giriniz : ");
+        int number = input.nextInt();
+        for (int i = 1;i<=number;i++){
+            result += (1.0/i);
+        }
+        System.out.println(result);
+    }
+}
